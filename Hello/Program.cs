eee353260b679244f5d84e6459a15da057c4d830
Mike Madden <michael.a.madden@nasa.gov>
2019-09-10 14:52:44 -0400

    First commit!

---
+++ Hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, what is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName +" " + myLastName+ "! I am a robot. I am your friend!");
+            Console.ReadLine();
+
+        }
+    }
+}
